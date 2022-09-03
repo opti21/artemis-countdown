c439de4db21d23d239e8dda26835f8cb75bd7684
opti21 <40129778+opti21@users.noreply.github.com>
2022-09-03 00:34:12 -0500

    add countdown

---
+++ src/pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200;400;900&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'source-code': ['"Source Code Pro"', 'monospace']
+      }
+    },
   },
   plugins: [],
 };
